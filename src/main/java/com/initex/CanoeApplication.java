520eedded0f17bd1c49c0e7320426a567112a71d
Ini <initex@inbox.lv>
2020-12-12 22:49:09 +0200

    Init

---
+++ src/main/java/com/initex/CanoeApplication.java
@@ -0,0 +1,13 @@
+package com.initex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanoeApplication.class, args);
+	}
+
+}
